b062bc6dfb6541485b0473ada29060e06eaf8fd3
Yongho <dydgh2011@gmail.com>
2023-09-01 03:29:07 +0900

    uploading existed files

---
+++ testing.js
@@ -0,0 +1,17 @@
+
+function testing(){
+    /*
+    let api_url = "https://api.rawg.io/api/games";
+    let params = {
+        'key': 'f51c080731c746f09f0a18e6f78f5360',
+        'page_size': 3,
+        'search': 'Minecraft',
+        'ordering': '-metacritic',
+        'platform': 4
+    };
+    let data = send_request(combine_url(api_url, params));
+    console.log(data[0])
+    */
+    window.alert("asdf");
+
+}
