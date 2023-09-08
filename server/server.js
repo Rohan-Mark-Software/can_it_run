36d15d7c7fa06221c04620bbf2a290328b4173ce
Yongho Nam <dydgh2011@gmail.com>
2023-09-09 03:44:16 +0900

    api testing

--- server/server.js
+++ server/server.js
@@ -1,11 +1,37 @@
 const express = require('express');
-const app = express();
-const PORT = 4000;  // Hard-code the port for now
+const mysql = require('mysql');
+const cors = require('cors');
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.use(cors());  // Enable CORS for all routes
+
+const db = mysql.createConnection({
+  host: 'mysql',
+  user: 'dydgh2011',
+  password: '14135647',
+  database: 'BENCHMARKS'
 });
 
+db.connect(err => {
+  if (err) {
+    throw err;
+  }
+  console.log('MySQL Connected...');
+});
+
+app.get('/getData', (req, res) => {
+  let sql = 'SELECT * FROM CPU';
+  db.query(sql, (err, results) => {
+    if (err) {
+      throw err;
+    }
+    res.json(results);
+  });
+});
+
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
